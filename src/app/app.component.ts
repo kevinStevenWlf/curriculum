c032c220faf18d4be516273e9daa0359ff2509cc
kevinStevenWlf <kevinsteven7497@gmail.com>
2025-05-31 19:04:00 -0500

    add about section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [ CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'curriculum';
+
+  techStack = [
+    { src: 'assets/tech/java.svg', alt: 'Java' },
+    { src: 'assets/tech/python.svg', alt: 'Python' },
+    { src: 'assets/tech/google-cloud.svg', alt: 'Google Cloud Platform' },
+    { src: 'assets/tech/angular.svg', alt: 'Angular' },
+    { src: 'assets/tech/nodejs.svg', alt: 'Node.js' },
+    { src: 'assets/tech/javascript.svg', alt: 'JavaScript' },
+    { src: 'assets/tech/html.svg', alt: 'HTML' },
+    { src: 'assets/tech/azure.svg', alt: 'Azure' },
+    { src: 'assets/tech/django.svg', alt: 'django' },
+    { src: 'assets/tech/mariadb.svg', alt: 'MariaDb' },
+    { src: 'assets/tech/flask.svg', alt: 'Flask' }
+  ];
+
+  get techStackInfinite() {
+    return [...this.techStack, ...this.techStack];
+  }
 }
